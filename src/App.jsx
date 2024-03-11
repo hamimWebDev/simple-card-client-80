7132d3f23c08b7ee445f65db2d47fa5bd4930aec
hamimDev <mdhamimhowladerasif@gmail.com>
2024-03-12 01:27:14 +0600

    sent from data in mongodb

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,35 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   return (
     <>
       <h1>Simple card</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" />
+        <br />
+        <input type="submit" value="Add User" />
+        <br />
+      </form>
     </>
   );
 }
